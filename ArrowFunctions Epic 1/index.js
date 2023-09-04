4dabac494785b3346f0c5fddd171084a67a385f9
MolliShearin <mollishearin@gmail.com>
2023-09-04 16:12:34 -0500

    Completed Basic Arrow Functions

---
+++ ArrowFunctions Epic 1/index.js
@@ -0,0 +1,16 @@
+//Example
+let double = n => n * 2;
+
+alert(double(5)); //10
+
+//Function that accepts two arguments and returns their product
+let product = (num1, num2) => num1 * num2
+console.log(product(3, 4)); //12
+
+//Fuction that takes in a string and returns it in uppercase
+let upperString = string => string.toUpperCase()
+console.log(upperString("I love cats"));
+
+//Function with a single parameter, calculating the square of the provided number
+let numberSquared = (number) => number * number
+console.log(numberSquared(3));
