ffec4f43f1781ef6f9b447539033552581636510
MolliShearin <mollishearin@gmail.com>
2023-09-23 16:50:15 -0500

    In class exercises 9-21-23

---
+++ Sprint2Exercises/InClassExercises 9-18-23/index.js
@@ -0,0 +1,8 @@
+//Exercise 1
+let myDiv = document.getElementById("myDiv");
+myDiv.style.backgroundColor = "lightblue";
+console.log(myDiv);
+
+let allParagraphs = document.getElementsByTagName("p");
+console.log(allParagraphs[0]);
+console.log(allParagraphs[1]);
